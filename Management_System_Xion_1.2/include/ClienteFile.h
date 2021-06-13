d8763de1ea5d7d8014f30872d4a3245c9dd3e85c
MATIAS <65511002+MATUKE23@users.noreply.github.com>
2021-06-13 09:13:05 -0300

    Add files via upload

---
+++ Management_System_Xion_1.2/include/ClienteFile.h
@@ -0,0 +1,18 @@
+#define FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+#ifndef CLIENTEFILE_H
+#define CLIENTEFILE_H
+
+
+class ClienteFile
+{
+	private:
+		FILE *pfile;
+
+	public:
+	bool grabarEnDisco_Cliente(int IDCliente);
+   //bool leerDeDisco_Cliente(Cliente_Proveedor_Propiedades &Cliente,int pos);
+
+};
+
+#endif // CLIENTEFILE_H

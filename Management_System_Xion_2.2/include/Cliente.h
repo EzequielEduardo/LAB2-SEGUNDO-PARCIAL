90f9fdeffeb14b9273b06f985a750c33f57b6912
EzequielEduardo <82185753+EzequielEduardo@users.noreply.github.com>
2021-06-22 22:27:58 -0300

    Add files via upload

---
+++ Management_System_Xion_2.2/include/Cliente.h
@@ -0,0 +1,33 @@
+#define FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+#ifndef CLIENTE_H
+#define CLIENTE_H
+#include "Persona.h"
+
+class Cliente:public Persona
+{
+		private:
+
+		char condicionFiscal[50];
+		char condicionDeCobro[50];
+
+
+
+	public:
+
+        ///sets
+
+		void settersPersona();
+
+        void setCondicionFiscal(const char*);
+        void setCondicionDeCobro(const char*);
+
+        ///gets
+		//void gettersPersona();
+        const char* getCondicionFiscal();
+		const char* getCondicionDeCobro();
+
+
+};
+
+#endif // CLIENTEOK_H
---
+++ Management_System_Xion_2.2/include/EmpleadoNegocio.h
@@ -0,0 +1,23 @@
+#define FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+#ifndef EMPLEADONEGOCIO_H
+#define EMPLEADONEGOCIO_H
+
+#include"Empleado.h"
+
+
+class  EmpleadoNegocio
+{
+	private:
+
+	public:
+
+	bool guardarDatos(Empleado);
+	Empleado* ExtraerDatos();
+	int CantidadDeSupplier();
+
+	void cargarCadena(char *pal, int tam);
+};
+
+
+#endif // EMPLEADONEGOCIO_H

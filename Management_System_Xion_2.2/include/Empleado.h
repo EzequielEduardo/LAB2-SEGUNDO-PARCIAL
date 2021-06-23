e592b4128a018c5865970722656554ede91db497
EzequielEduardo <82185753+EzequielEduardo@users.noreply.github.com>
2021-06-22 22:25:59 -0300

    Add files via upload

---
+++ Management_System_Xion_2.2/include/Empleado.h
@@ -0,0 +1,28 @@
+#define FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+#ifndef EMPLEADO_H
+#define EMPLEADO_H
+#include"Persona.h"
+
+class Empleado:public Persona
+{
+		private:
+
+
+		char turno[50];
+		float sueldo;
+
+	public:
+
+
+        ///sets
+
+        void setTurno(const char *cadena);
+		void setSueldo(const float);
+
+        ///gets
+
+        const char* getTurno();
+		const float getSueldo();
+};
+#endif // EMPLEADO_H
---
+++ Management_System_Xion_2.2/src/MenuPrincipal.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+#include<clocale>
+#include<cstdlib>
+#include<cstring>
+#include <cstdio>
+#include<string.h>
+#include"MenuPrincipal.h"
+#include"ClienteVista.h"
+#include"ProveedorVista.h"
+#include"EmpleadoVista.h"
+using namespace std;
+
+
+void MenuPrincipal::menuPrincipal()
+{
+    int opc;
+    setlocale(LC_ALL,"Spanish");
+    system("color 02");
+
+    do
+    {
+        cout<<"MENU PRINCIPAL"<<endl<<endl;
+        cout<<"1-GESTION DE CLIENTES"<<endl;
+        cout<<"2-GESTION DE PROVEEDORES"<<endl;
+        cout<<"3-GESTION DE EMPLEADOS"<<endl;
+        cout<<"0-SALIR DEL PROGRAMA"<<endl<<endl;
+        cout<<"Ingresar Opcion: ";
+        cin>>opc;
+
+        system("cls");
+        switch(opc)
+        {
+
+		case 1:
+			ClienteVista obj;
+			obj.menuClientes();
+            system("cls");
+		break;
+
+		case 2:
+
+         ProveedorVista obj1;
+         obj1.menuProveedores();
+            system("cls");
+		break;
+
+		case 3:
+           EmpleadoVista obj2;
+           obj2.menuEmpleados();
+
+
+            system("cls");
+            break;
+
+
+
+        case 0:
+            cout<<"-----FIN DEL PROGRAMA-----";
+            break;
+
+        default:
+            break;
+
+        }
+
+    }
+    while(opc!=0);
+
+
+}
+
